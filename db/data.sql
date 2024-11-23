c7a136afa6f6cdaf0b5c02dec41e4ee85011dc25
David LaJambe <dlajambe@gmail.com>
2024-11-23 09:49:40 -0500

    Add content to schema file

--- db/data.sql
+++ db/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO sensors (sampling_period) VALUES
-(1.0),
-(10.0)
+INSERT INTO sensors (signal_unit, sampling_period, sampling_period_time_unit) VALUES
+( 'celsius', 1, 'seconds'),
+('m3_per_hour', 100, 'milliseconds')
--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,15 @@
+CREATE TYPE time_unit AS ENUM (
+    'milliseconds',
+    'seconds',
+    'minutes');
+
+CREATE TYPE signal_unit AS ENUM (
+    'celsius',
+    'm3_per_hour',
+    'percentage');
+
 CREATE TABLE sensors (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    sampling_period BIGINT
-);
+    signal_unit signal_unit NOT NULL,
+    sampling_period BIGINT NOT NULL,
+    sampling_period_time_unit time_unit NOT NULL);
