0882e0f612e0a168f918ebe92e758e8f9c4f931b
David LaJambe <dlajambe@gmail.com>
2024-12-14 11:11:24 -0500

    Temp commit

--- src/intTest/java/com/control_ops/control_system/services/SensorServiceIT.java
+++ src/intTest/java/com/control_ops/control_system/services/SensorServiceIT.java
@@ -1,28 +1,50 @@
 package com.control_ops.control_system.services;
 
-import com.control_ops.control_system.ControlSystemApplication;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = ControlSystemApplication.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SensorServiceIT {
 
-/*
+    @LocalServerPort
+    private Integer port;
+
     private final SensorService sensorService;
 
     @Autowired
     SensorServiceIT(final SensorService sensorService) {
         this.sensorService = sensorService;
     }
-*/
+
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            "postgres:16-alpine"
+    );
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
 
     @Test
     void can_get_sensor_with_valid_id() {
